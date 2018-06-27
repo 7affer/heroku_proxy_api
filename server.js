24ddd4600ab38176bedbdcb3f275bf31c2efdf70
Bartłomiej Wilk <bartek.wilk@outlook.com>
2018-06-27 02:51:11 +0200

    server

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const app = express();
+const port = process.env.PORT || '8080';
+
+app.use('/api', (req, res) => {
+  res.status(200).send('api response');
+});
+
+app.set('port', port);
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', socket => {
+  console.log('socket connected');
+
+  socket.emit('hello', { message: 'world' });
+});
+
+app.set('io', io);
+
+server.listen(port);
+
+module.exports = server;
