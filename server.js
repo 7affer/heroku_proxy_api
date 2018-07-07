c9d7ec80b47d106b4a88ef160a7f040d0ccbacba
Bartłomiej Wilk <bartek.wilk@outlook.com>
2018-07-07 20:10:17 +0200

    added-sticky-session

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const sticky = require('socketio-sticky-session');
 
 const app = express();
 const port = process.env.PORT || '8080';
+
+var stickySessionOptions = {
+  proxy: true, //activate layer 4 patching
+  header: 'x-forwarded-for', //provide here your header containing the users ip
+  num: 2, //count of processes to create, defaults to maximum if omitted
+  sync: {
+    isSynced: true, //activate synchronization
+    event: 'mySyncEventCall' //name of the event you're going to call
+  }
+}
 
 app.use('/api', (req, res) => {
   res.status(200).send('api response');
@@ -11,18 +22,20 @@ app.use('/api', (req, res) => {
 
 app.set('port', port);
 
-const server = http.createServer(app);
+const stictyServer = sticky(options, () => {
+  const server = http.createServer(app);
 
-const io = socketIO(server);
+  const io = socketIO(server);
 
-io.on('connection', socket => {
-  console.log('socket connected');
+  io.on('connection', socket => {
+    console.log('socket connected');
 
-  socket.emit('hello', { message: 'world' });
+    socket.emit('hello', { message: 'world' });
+  });
+
+  app.set('io', io);
 });
 
-app.set('io', io);
-
-server.listen(port);
+stictyServer.listen(port);
 
 module.exports = server;
